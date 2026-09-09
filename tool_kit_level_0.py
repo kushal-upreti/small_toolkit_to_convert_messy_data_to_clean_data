04b81f44e73e1269e51099fca38d1c506e82aee4
Kushal Upreti <kushalupreti66@gmail.com>
2026-09-09 15:32:22 +0545

    wrap level 0 task in function

--- tool_kit_level_0.py
+++ tool_kit_level_0.py
@@ -4,15 +4,21 @@ import csv
 
 filepath = "/data/Files/Intern_Docs/Task_3/messy_people.csv"
 
-people_messy_csv = open(filepath)
+def dict_list(filepath):
+    people_messy_csv = open(filepath)
 
-csv_data = csv.DictReader(people_messy_csv)
+    csv_data = csv.DictReader(people_messy_csv)
 
-dict_list = list(csv_data)
+    csv_dict_list = list(csv_data)
+    return csv_dict_list
 
-for i in range(len(dict_list)):
-    if i > 2:
-        break
-    print(dict_list[i])
 
-print(len(dict_list))
+if __name__ == "__main__":
+    csv_data_list = dict_list(filepath)
+
+    for i in range(len(csv_data_list)):
+        if i > 2:
+            break
+        print(csv_data_list[i])
+
+    print(len(csv_data_list))
